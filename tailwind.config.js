d08a203fda4c53a1e4b8e3c8de8643c81ab89854
ghazale fattahi <ghazale8181fattahi@gmail.com>
2023-12-31 11:44:02 +0330

    default config and styles added

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./public/**/*.{html,js}',],
+  content: ['./public/**/*.{html,js}'],
   theme: {
-    extend: {},
+    extend: {
+colors:{
+  primary:'#FF6363',
+  secondary:{
+    100:'#E2E2D5',
+    200:'#888883',
+  }
+},
+    fontFamily:{
+      body:['Nunito','Barlow']
+
+    }
+    },
   },
   plugins: [],
 }
