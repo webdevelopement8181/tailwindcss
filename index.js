7b203901ec69648d8fe2e09e0ea0be1ffd83937e
ghazale fattahi <ghazale8181fattahi@gmail.com>
2024-06-01 12:10:23 +0330

    updated the structure

--- index.js
+++ index.js
@@ -1,12 +1,7 @@
-const burger=document.querySelector('#burger');
-const menu=document.querySelector('#menu');
-// burger.addEventListener('click',() => {
-//     if(menu.classList.contains('hidden')){
-//         menu.classList.remove('hidden');
-//     }else{
-//         menu.classList.add('hidden');
-//     }
-// })
-burger.addEventListener('click', ()=>{
-    menu.classList.toggle('hidden');
-})
+document.addEventListener("DOMContentLoaded",() => {
+    const menuToggle =document.getElementById("menu-toggle");
+    const navbarSticky=document.getElementById("navbar-sticky");
+    menuToggle.addEventListener("click",() => {
+    navbarSticky.classList.toggle("hidden");
+    });
+});
